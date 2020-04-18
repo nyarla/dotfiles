dfddab631c1de3e291568e589e4327d331e3352a
nyarla <nyarla@kalaclista.com>
2020-04-18 14:52:01 +0900

    new: added new feature to rofi launcher && added openbox configuration for nested Xorg instance

--- scripts/rofi-application-launch.sh
+++ scripts/rofi-application-launch.sh
@@ -14,12 +14,72 @@ filter() {
         -color-active "#191919,#FFFFFF,#333333,#00CCFF,#000000" 
 }
 
+applications() {
+  cat <<EOF
+# Browser
+firefox
+transmission-gtk
+
+# Files
+caja
+engrampa
+eom
+atril
+pluma
+
+# Musics
+qjackctl
+bitwig-studio
+carla
+
+# Multimedia
+pavucontrol
+arandr
+calibre
+deadbeef
+
+# Office
+mate-calc
+gucharmap
+gimp
+inkscape
+peek
+com.github.philip-scott.spice-up
+keepassxc
+qtpass
+simple-scan
+
+# VM
+VirtualBox
+
+# Terminal
+mlterm
+
+# Utils
+dconf-editor
+magnify
+mullvad-gui
+connman-gtk
+Xephyr
+
+# Scripts
+kindle
+FLStudio
+adobe
+mp3tag
+EOF
+}
+
 main() {
-  local APPS="$(ls /run/current-system/sw/bin | grep -v "^#" | grep -v "^$" | sort | uniq)"
+  local APPS="$(applications | grep -v "^#" | grep -v "^$" | sort | uniq)"
   local SEL="$(echo $APPS | tr ' ' "\n" | filter)"
 
-  if type $SEL; then
-    exec $SEL
+  if test -x ~/local/dotfiles/bin/$SEL ; then
+    exec ~/local/dotfiles/bin/$SEL
+  fi
+
+  if test -x /run/current-system/sw/bin/$SEL ; then
+    exec /run/current-system/sw/bin/$SEL
   fi
 }
 
