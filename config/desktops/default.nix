b70414beba2cd9f1e8d95ddefbaaa4f951c81237
nyarla <nyarla@kalaclista.com>
2021-12-20 09:57:51 +0900

    new: use sx as Xorg starter

---
+++ config/desktops/default.nix
@@ -0,0 +1,30 @@
+{ pkgs, ... }:
+let
+  sxhkdrc = pkgs.writeText "sxhkdrc" ''
+    super + l
+      i3lock-fancy
+
+    super + alt + l
+      mlterm
+  '';
+
+  sxrc = pkgs.writeShellScript "xinitrc" ''
+    for rc in $(ls /etc/profile.d); do
+      . /etc/profile.d/$rc
+    done
+
+    export LANG=ja_JP.UTF-8
+    export LC_ALL=ja_JP.UTF-8
+    export XDG_SESSION_TYPE=x11
+
+    systemctl --user import-environment DISPLAY XAUTHORITY DBUS_SESSION_BUS_ADDRESS XDG_SESSION_ID
+    systemctl --user start graphical-session.target
+
+    dbus-update-activation-environment --systemd --all
+
+    sxhkd -c ${sxhkdrc} &
+    xsetroot -cursor_name left_ptr
+
+    exec openbox-session
+  '';
+in { xdg.configFile."sx/sxrc".source = "${sxrc}"; }
