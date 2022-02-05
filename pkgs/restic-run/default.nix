295a8b078699a6eb75265cc02a0226e0a86ffa3c
nyarla <nyarla@kalaclista.com>
2022-02-05 16:06:49 +0900

    refactor(restic-run): rewrite scripts

--- pkgs/restic-run/default.nix
+++ pkgs/restic-run/default.nix
@@ -1,48 +1,54 @@
 { runCommand, writeShellScript, restic, rclone }:
 let
 
-  resticInit = ''
+  restic-run = writeShellScript "restic-run.sh" ''
     set -euo pipefail
 
     if test -z "''${1:-}" ; then
-      echo "Usage: ''${0} <name>" >&2
+      echo "Usage: ''${0} <name> [restic args...]" >&2
       exit 1
     fi
 
     export HOME=/home/nyarla
     export RESTIC_PASSWORD_FILE=$HOME/.config/rclone/restic
-    export RESTIC_FORGET_ARGS="--prune --keep-daily 7 --keep-weekly 2 --keep-monthly 3"
-    export RESTIC_BACKUP_ARGS="--exclude-file $HOME/.config/rclone/ignore"
+
     export RESTIC_REPOSITORY=rclone:Teracloud:Restic/$(hostname)/''${1}
 
-    restic-run() {
-      args=(''${@})
-      unset args[0]
-      unset args[1]
-      args=("''${args[@]}")
-      
-      ${restic}/bin/restic ''${1:-} -o=rclone.program="${rclone}/bin/rclone" ''${args[@]}
-    }
+    shift 1
+
+    cmd=''${1:-}
+    shift 1
+
+    args=(''${@})
+
+    exec ${restic}/bin/restic $cmd -o=rclone.program="${rclone}/bin/rclone" ''${args[@]}
   '';
 
-  resticWrapper = writeShellScript "restic.sh" ''
-    ${resticInit}
+  restic-backup = writeShellScript "restic-backup.sh" ''
+    set -euo pipefail
 
-    restic-run $@ 
-  '';
+    export RESTIC_FORGET_ARGS="--prune --keep-daily 7 --keep-weekly 2 --keep-monthly 3"
+    export RESTIC_BACKUP_ARGS="--exclude-file $HOME/.config/rclone/ignore"
 
-  resticBackupScript = writeShellScript "restic-backup.sh" ''
-    ${resticInit}
+    if test -z "''${1:-}" ; then
+      echo "Usage: ''${0} <name> [restic args...]" >&2
+      exit 1
+    fi
 
-    restic-run unlock --remove-all
-    restic-run rebuild-index
-    restic-run prune
-    restic-run backup $RESTIC_BACKUP_ARGS $@
-    restic-run forget $RESTIC_FORGET_ARGS
+    repo=''${1:-}
+    shift 1
+
+    args=(''${@})
+
+    ${restic-run} $repo unlock --remove-all
+    ${restic-run} $repo rebuild-index
+    ${restic-run} $repo prune
+    ${restic-run} $repo backup $RESTIC_BACKUP_ARGS ''${args[@]}
+    ${restic-run} $repo forget $RESTIC_FORGET_ARGS
   '';
 in runCommand "restic-run" { } ''
   mkdir -p $out/bin
-  cp ${resticWrapper} $out/bin/restic-run
-  cp ${resticBackupScript} $out/bin/restic-backup
+  cp ${restic-run} $out/bin/restic-run
+  cp ${restic-backup} $out/bin/restic-backup
   chmod +x $out/bin/*
 ''
