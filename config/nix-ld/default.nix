50eb68a064991293475b7686525330d3b42c87f4
nyarla <nyarla@kalaclista.com>
2022-06-22 17:23:02 +0900

    chore(nix-ld): code improvement

--- config/nix-ld/default.nix
+++ config/nix-ld/default.nix
@@ -1,11 +1,18 @@
-{ config, pkgs, lib, ... }: {
+{ pkgs, lib, ... }:
+let
+  packages = with pkgs; [
+    # for standalone binary
+    stdenv.cc.cc.lib
+    stdenv.cc.libc
+  ];
+in {
   xdg.configFile."profile.d/nix-ld.sh" = {
     text = ''
       export NIX_LD_LIBRARY_PATH="$NIX_LD_LIBRARY_PATH''${NIX_LD_LIBRARY_PATH:+:}${
-        lib.makeLibraryPath config.home.packages
+        lib.makeLibraryPath packages
       }"
 
-      export NIX_LD_LIBRARY_PATH=$( echo $NIX_LD_LIBRARY_PATH | tr ':' "\n" | sort | uniq | tr "\n" ":" | sed 's/:$//' )
+      export NIX_LD_LIBRARY_PATH=$( echo $NIX_LD_LIBRARY_PATH | tr ':' "\n" | sort -u | tr "\n" ":" | sed 's/:$//' )
     '';
   };
 }
